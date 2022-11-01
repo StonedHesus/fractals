a61e4e225bf45f60de4cf811770e2a4ebb435338
stonedhesus <ionescu_paul@outlook.com>
2022-11-01 16:01:12 +0100

    Established the first iteration of the project's architecture.

---
+++ src/main/java/fractals/main/settings/Settings.java
@@ -0,0 +1,11 @@
+// The package in which the current Java compilation unit is to be found.
+package fractals.main.settings;
+// Imports from existing Java libraries, classes and interfaces.
+
+// Import from custom libraries, classes and interfaces.
+
+/**
+ * @author Andrei-Paul Ionescu
+ */
+public interface Settings {
+}
---
+++ src/test/java/fractals/main/math/complexnumbers/ComplexNumberTest.java
@@ -0,0 +1,19 @@
+// The package in which the current Java compilation unit is to be found.
+package fractals.main.math.complexnumbers;
+// Imports from existing Java libraries, classes and interfaces.
+
+// Import from custom libraries, classes and interfaces.
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Andrei-Paul Ionescu
+ */
+public class ComplexNumberTest {
+
+    @Test()
+    @DisplayName("This is a test whose role is to test the tests.")
+    public void testFoo() {assertTrue(true, "");}
+}
